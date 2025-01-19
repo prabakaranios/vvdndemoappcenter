8476669afcffbe20f27b21a472a4e6f6c935c96a
prabakaran kuppusamy <prabakaran.kuppusamy@vvdntech.in>
2025-01-19 13:13:59 +0530

    Initial Commit

---
+++ vvdndemoapp/vvdndemoappApp.swift
@@ -0,0 +1,17 @@
+//
+//  vvdndemoappApp.swift
+//  vvdndemoapp
+//
+//  Created by vvdn on 19/01/25.
+//
+
+import SwiftUI
+
+@main
+struct vvdndemoappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
